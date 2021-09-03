a1dd92eccf2853aed7aad45fc6cd8b6d6dac9da3
Nicolai Berk <nicolai.berk@gmail.com>
2021-09-03 12:02:44 +0200

    KSearch STM

--- code/news_framing_from_scratch.R
+++ code/news_framing_from_scratch.R
@@ -7,11 +7,12 @@ library(lubridate)
 library(tidytext)
 
 # load the preselected migration articles
-mig_texts_raw <- fread(here('data/_migration_articles_new.csv'), encoding = 'UTF-8') %>% as_tibble()
+# mig_texts_raw <- fread(here('data/_migration_articles_new.csv'), encoding = 'UTF-8') %>% as_tibble()
+mig_texts_raw <- fread(here('data/_migration_articles_BERT.csv'), encoding = 'UTF-8') %>% as_tibble()
 
 
 # use subsample for now
-# mig_texts_raw <- mig_texts_raw[sample(1:nrow(mig_texts_raw), 10000),]
+# mig_texts_raw <- mig_texts_raw[sample(1:nrow(mig_texts_raw), 1000),]
 
 # fix dates
 mig_texts_raw$paper[mig_texts_raw$paper == "weltonline"] <- "welt"
@@ -28,7 +29,7 @@ mig_texts_raw <- mig_texts_raw[mig_texts_raw$date_new > as.Date('2010-01-01'),]
 # length(unique(mig_texts_raw$url)) # some duplicates
 
 ## load german stopwords
-# german_stopwords <- data.frame(word = c(stopwords("de"), 'dass', 'sagte', 'sagt', 'sei'), stringsAsFactors = F)
+german_stopwords <- data.frame(word = c(stopwords::stopwords("de"), 'dass', 'sagte', 'sagt', 'sei'), stringsAsFactors = F)
 
 # ## preprocess: sort out non-unique cases, tokenize, remove stopwords
 # mig_texts_tidy <- mig_texts_raw %>%
@@ -52,16 +53,58 @@ mig_texts_raw <- mig_texts_raw[mig_texts_raw$date_new > as.Date('2010-01-01'),]
 
 
 # alternative approach: use stmprinter to find right k of topics
-library(stmprinter)
+# library(stmprinter)
+# 
+# mig_texts_raw <- mig_texts_raw %>% 
+#   mutate(quarter = floor_date(date_new, unit = 'quarters'))
+# 
+# ## preprocess
+# processed <- textProcessor(
+#   documents = mig_texts_raw$text,
+#   metadata = mig_texts_raw, 
+#   language = 'german'
+# )
+# 
+# out <- prepDocuments(
+#   documents = processed$documents,
+#   vocab = processed$vocab,
+#   meta = processed$meta
+# )
+# 
+# set.seed(42)
+# 
+# print(paste('Started at:', Sys.time()))
+# 
+# stm_models <- many_models(
+#   K = c(40, 60, 80, 110, 150),
+#   documents = out$documents,
+#   vocab = out$vocab, 
+#   data = out$meta,
+#   N = 1,
+#   runs = 10,
+#   cores = 3
+# )
+# 
+# print_models(
+#   stm_models, mig_texts_raw$text,
+#   file = here('code/mig_stm_runs.pdf'),
+#   title = "Migration frames"
+# )
+# 
+# print(paste('Finished at:', Sys.time()))
 
-mig_texts_raw <- mig_texts_raw %>% 
-  mutate(month = floor_date(date_new, unit = 'months'))
+
+## alternative 2: use stm package to decide on K
+# mig_texts_raw <- mig_texts_raw %>%
+#   mutate(quarter = floor_date(date_new, unit = 'quarters'))
 
 ## preprocess
 processed <- textProcessor(
   documents = mig_texts_raw$text,
-  metadata = mig_texts_raw, 
-  language = 'german'
+  metadata = mig_texts_raw,
+  language = 'german',
+  customstopwords = german_stopwords$word,
+  stem = F
 )
 
 out <- prepDocuments(
@@ -72,19 +115,25 @@ out <- prepDocuments(
 
 set.seed(42)
 
-stm_models <- many_models(
-  K = seq(10, 100, 10),
-  documents = out$documents,
-  vocab = out$vocab,
-  prevalence = ~ paper + month, 
-  data = out$meta,
-  N = 5,
-  runs = 100,
-  cores = 1 # bc f windows
-)
+print(paste('Started at:', Sys.time()))
 
-print_models(
-  stm_models, mig_texts_raw$text,
-  file = here('_sc/_framing/mig_stm_runs.pdf'),
-  title = "Migration frames"
-)
+## Search K
+print("Searching right number of topics...")
+kResult <- searchK(out$documents, out$vocab, K = c(100, 60, 30, 10), init.type = "Spectral", prevalence =~ date_new + paper, data = out$meta)
+
+# plot disgnostics
+jpeg("paper/vis/plot1_kSearch_broad.jpeg")
+plot(kResult)
+dev.off()
+
+# plot coherence-exclusivity
+jpeg("paper/vis/plot2_kSearch_broad.jpeg")
+plot(kResult$results$semcoh, kResult$results$exclus, xlab = 'Semantic Coherence', ylab = 'Exclusivity')
+text(kResult$results$semcoh, kResult$results$exclus, labels = paste("K", kResult$results$K), pos = 1)
+dev.off()
+
+# coherence-exclusivity table
+save(kResult$results, file = 'paper/vis/table_ksearch_broad.RData')
+
+
+print(paste('Finished at:', Sys.time()))
